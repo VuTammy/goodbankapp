4fc7c2ea3a9574e09a14b29ae1f207cb13e5be97
Tammy Vu <tammysvu@gmail.com>
2022-10-26 14:47:26 -0500

    url

--- dal.js
+++ dal.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url         = 'mongodb://localhost:27017';
+const url         = 'mongodb+srv://tammyvu:<password>@cluster0.dh0cbrq.mongodb.net/?retryWrites=true&w=majority';
 let db            = null;
  
 // connect to mongo
